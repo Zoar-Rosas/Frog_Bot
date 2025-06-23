90a9f39c0ef7b5c8c1f2e875fb8fb32fd4dbfe92
Zoar Nava <zoar.rosas@outlook.com>
2025-06-23 14:35:32 -0600

    🐍📡 Módulo central del bot

--- src/main.py
+++ src/main.py
@@ -28,7 +28,7 @@ async def main_app():
         ejecutor = EjecutorOrdenes(cliente, periodo)
         stream = RealTimeCandlesOTC(cliente, analizador, ejecutor, periodo)
 
-        # Manejar 'Ctrl+C'
+        # Manejar salida con Ctrl+C
         def handle_interrupt(sig, frame):
             print(f"\n{Colors.INFO}Deteniendo bot...\033[0m")
             raise KeyboardInterrupt
